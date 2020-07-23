df406890aeb06f1f01b67b28db2624db75cd6605
Paulo Henrique <paulohenrique@MacBook-Air-de-Paulo.local>
2020-07-23 01:30:49 -0300

    Add form input to add task

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 
 const TodoForm = () => {
     return (
-        <div>
-            <h1>Form todo</h1>
+        <div role='form' className='todoForm'>
+            <div className='col-xs-12 col-sm-9 col-md-10'>
+                <input id='description' className='form-control' 
+                    placeholder='Add task'></input>
+            </div>
+            <div className='col-xs-12 col-sm-3 col-md-2'>
+                <button className='btn btn-primary'>
+                    <i className='fa fa-plus'></i>
+                </button>
+            </div>
+
         </div>
     );
 }
