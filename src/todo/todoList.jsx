2f5661929462340b3756dd13253171f2f203904a
Paulo Henrique <paulohenrique@MacBook-Air-de-Paulo.local>
2020-07-27 20:39:24 -0300

    Add button behavior(done, rollback, delete)

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -9,9 +9,11 @@ export default props => {
 
         return list.map( todo => (
             <tr key={todo._id}>
-                <td>{todo.description}</td>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconItem type='danger' icon='trash-o' onClick={ () => props.handleRemove(todo)}></IconItem>
+                    <IconItem type='success' icon='check' onClick={ () => props.handleMarkAsDone(todo)} hide={todo.done}></IconItem>
+                    <IconItem type='warning' icon='undo' onClick={ () => props.handleMarkAsPending(todo)} hide={!todo.done}></IconItem>
+                    <IconItem type='danger' icon='trash-o' onClick={ () => props.handleRemove(todo)} hide={!todo.done}></IconItem>
                 </td>
             </tr>
         ))
